c53103b29e975032c2be6266e0b01ed87c5c5b3e
Gandham <539285@cognizant.com>
2018-02-09 20:15:57 +0530

    password validation and match

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -24,7 +24,71 @@ angular.module('userController', ['userServices'])
                 app.errorMsg = "please ensure your data";
             }
         };
+       this.checkUsername = function(regData){
+          app.checkingusername = true ; 
+           app.usernameMsg = false;
+           app.usernameInvalid = false ; 
+        User.checkusername(app.regData).then(function(data){
+         if(data.data.success){
+            app.checkingusername = false ; 
+            app.usernameInvalid = false ; 
+            app.usernameMsg = data.data.message;
+         }else{
+            app.checkingusername = true ; 
+            app.usernameInvalid = true ; 
+            app.usernameMsg = data.data.message;
+  
+         }
+        });
+
+       }
+       this.checkEmail = function(regData){
+        app.checkingEmail = true ; 
+        app.emailMsg = false;
+        app.emailInvalid = false ; 
+        User.checkemail(app.regData).then(function(data){
+            if(data.data.success){
+                app.checkingemail = false ; 
+                app.emailInvalid = false ; 
+                app.emailMsg = data.data.message;
+             }else{
+                app.checkingemail = true ; 
+                app.emailInvalid = true ; 
+                app.emailMsg = data.data.message;
+      
+             }
+        });
+
+       }   
     })
+    .directive('match', function() {
+        return {
+          restrict: 'A',
+          controller:   function($scope){
+          $scope.confirmed = false;
+            $scope.doConfirm = function(values){
+               values.forEach(function(ele){
+                   if ($scope.confirm == ele) {
+                       $scope.confirmed = true;
+                   } else {
+                    $scope.confirmed = false;
+                    }
+                                 });
+           }
+          },
+          link: function(scope,element,attrs){
+           
+            attrs.$observe('match', function(){
+                scope.matches = JSON.parse(attrs.match); 
+                scope.doConfirm(scope.matches);
+              });
+             scope.$watch('confirm', function(){
+                scope.matches = JSON.parse(attrs.match); 
+                scope.doConfirm(scope.matches);
+               }); 
+          }     
+         };
+      })
     .controller('facebookCtrl', function($routeParams, Auth, $location, $window) {
         var app = this;
         if ($window.location.pathname == '/facebookerror') {
